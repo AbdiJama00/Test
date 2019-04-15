78ec549b08cd051ec9a1f1c360dd7d67c25d6150
Abdi Jama <abdullahisjama@gmail.com>
2019-04-16 11:28:42 +1200

    commit image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
-import Img from "gatsby-image"
+import Image from "gatsby-image"
 import styled from "styled-components"
 
 import Bio from "../components/bio"
