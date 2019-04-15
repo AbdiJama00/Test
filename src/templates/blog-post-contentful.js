ca2a1e95feae88788bb5e8435d399cb30cb2e7e1
Abdi Jama <abdullahisjama@gmail.com>
2019-04-16 11:27:08 +1200

    commit image

--- src/templates/blog-post-contentful.js
+++ src/templates/blog-post-contentful.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
-import Img from "gatsby-Image"
+import Image from "gatsby-image"
 
 import Bio from "../components/bio"
 import Layout from "../components/layout"
