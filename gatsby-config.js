90f87e4e35eb784ead1766ec67b1835c0f794742
Abdi Jama <abdullahisjama@gmail.com>
2019-04-16 14:18:21 +1200

    text change commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `BIOZONE ACADEMY`,
+    author: `Abdullahi Jama`,
+    description: `A starter site.`,
+    siteUrl: ``,
     social: {
-      twitter: `kylemathews`,
+      twitter: ``,
     },
   },
   plugins: [
